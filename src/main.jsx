4b3a2b636f5ccf7108c166847997ab31a537b85e
Hangyul Park <hydrodddd@gmail.com>
2026-09-11 23:11:10 -0400

    first component yay

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Card from './Card.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Card />
   </StrictMode>,
 )
